2ddca8ad791a362fb0f158ec533cd0938de7890c
martinhsl68hw <mmholl@ic.ac.uk>
2023-11-06 22:46:27 +0000

    Second commit

--- src/main/java/MainServlet.java
+++ src/main/java/MainServlet.java
@@ -4,21 +4,13 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.stream.Collectors;
+
 
 @WebServlet(urlPatterns={"/patients","/doctors"},loadOnStartup = 1)
 public class MainServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType("text/html");
-        resp.getWriter().write("Hello, worldss!");
+        resp.getWriter().write("<b>Hello, world!</b>");
     }
-
-    @Override
-    public void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String reqBody=req.getReader().lines().collect(Collectors.joining(System.lineSeparator()));
-        resp.setContentType("text/html");
-        resp.getWriter().write("Thank you client! "+reqBody);
-    }
-
 }
