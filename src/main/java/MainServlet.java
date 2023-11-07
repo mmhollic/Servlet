5bbdabf3beb41febb8fd07bd1026afd9cf774384
martinhsl68hw <mmholl@ic.ac.uk>
2023-11-07 09:41:21 +0000

    Second commit

--- src/main/java/MainServlet.java
+++ src/main/java/MainServlet.java
@@ -5,12 +5,11 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-
 @WebServlet(urlPatterns={"/patients","/doctors"},loadOnStartup = 1)
 public class MainServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType("text/html");
-        resp.getWriter().write("<b>Hello, world!</b>");
+        resp.getWriter().write("<b>Hello, Software teams!</b>");
     }
 }
